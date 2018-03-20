9e687499a582a447c71e287361617b3cf1eb310e
Saroj Gyawali <saroj.gyawalis4@gmail.com>
2018-03-19 21:03:38 -0500

    Some changes in classes

--- src/com/finco/account/EntryFactory.java
+++ src/com/finco/account/EntryFactory.java
@@ -1,5 +1,22 @@
 package com.finco.account;
 
-public class EntryFactory {
+import java.time.LocalDate;
 
+public class EntryFactory implements IEntryFactory{
+	
+	private static IEntryFactory myInstance = new EntryFactory();
+	
+	private EntryFactory() {
+		
+	}
+	
+	public static IEntryFactory getInstance() {
+		return myInstance;
+	}
+
+	public IEntry getEntry(String name, LocalDate date, double amount, EntryType entryType) {
+		
+		return new Entry(name, date, amount, entryType);
+		
+	}
 }
