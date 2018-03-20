d476e9666141dcf4331e62e8ee849178e65e3884
AmanDev <aman.yahway@gmail.com>
2018-03-19 22:02:11 -0500

    AutomatedEmail

--- src/com/finco/customer/ICustomer.java
+++ src/com/finco/customer/ICustomer.java
@@ -1,9 +1,10 @@
 package com.finco.customer;
 
+ import com.finco.account.Entry;
 import com.finco.account.IAccount;
 
 public interface ICustomer {
 	public void addAccount(IAccount acct);
 	public void removeAccount(IAccount acct);
-	public void sendEmailToCustomer();
+	public void sendEmailToCustomer(Entry entry);
 }
