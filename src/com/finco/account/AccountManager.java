cebd827c4c71ed07ac3414a65a1ae92326917a7f
Saroj Gyawali <saroj.gyawalis4@gmail.com>
2018-03-19 20:32:07 -0500

    Account Classes
    Classes of Account package. com.finco.account

---
+++ src/com/finco/account/AccountManager.java
@@ -0,0 +1,11 @@
+package com.finco.account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountManager {
+	
+	private List<IAccount> accountList = new ArrayList<IAccount>();
+	
+
+}
---
+++ src/com/finco/account/DepositStrategy.java
@@ -0,0 +1,5 @@
+package com.finco.account;
+
+public class DepositStrategy {
+
+}
