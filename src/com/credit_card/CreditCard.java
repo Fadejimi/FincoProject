103d1fddcb88d3ff872929291886b90dbb0fcab3
AmanDev <aman.yahway@gmail.com>
2018-03-20 22:55:52 -0500

    a

--- src/com/credit_card/CreditCard.java
+++ src/com/credit_card/CreditCard.java
@@ -2,14 +2,10 @@ package com.credit_card;
 
 import com.finco.customer.ACustomer;
 
-public class CreditCard extends{
+public class CreditCard{
 
-	String ccNumber;
-	String 
-	public CreditCard(String name, String state, String street, String city,
-			String zip, String email) {
-		super(name, state, street, city, zip, email);
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		
 	}
 
 }
