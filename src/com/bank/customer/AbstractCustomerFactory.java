687cd1e1a53e9d8458985e7bf21b692e98ea6493
Saroj Gyawali <saroj.gyawalis4@gmail.com>
2018-03-20 21:16:59 -0500

    Changes in factory methods

--- src/com/bank/customer/AbstractCustomerFactory.java
+++ src/com/bank/customer/AbstractCustomerFactory.java
@@ -1,24 +1,25 @@
+// Dont need this anymore
+
 package com.bank.customer;
 
-import com.bank.account.SavingsAccountFactory;
 import com.finco.customer.ICustomerFactory;
 
 public class AbstractCustomerFactory {
-	
-private static ICustomerFactory customerFactory = null;
-	
-	private AbstractCustomerFactory() {
-		
-	}
-	
-	public static ICustomerFactory getFactory(String type) {
-		if(type.equals("personal")) {
-			customerFactory = PersonalCustomerFactory.getInstance();
-		} else if(type.equals("company")) {
-			customerFactory = CompanyCustomerFactory.getInstance();
-		}
-		return customerFactory;
-	}
+
+//private static ICustomerFactory customerFactory = null;
+//	
+//	private AbstractCustomerFactory() {
+//		
+//	}
+//	
+//	public static ICustomerFactory getFactory(String type) {
+//		if(type.equals("personal")) {
+//			customerFactory = PersonalCustomerFactory.getInstance();
+//		} else if(type.equals("company")) {
+//			customerFactory = CompanyCustomerFactory.getInstance();
+//		}
+//		return customerFactory;
+//	}
 
 
 }
