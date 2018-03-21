03eb1e1af2c8f010aaa1aab0ac94a2391b50121c
AmanDev <aman.yahway@gmail.com>
2018-03-20 20:28:47 -0500

    credit Card Factory

--- src/com/credit_card/account/GoldFactory.java
+++ src/com/credit_card/account/GoldFactory.java
@@ -1,5 +1,16 @@
 package com.credit_card.account;
 
-public class GoldFactory {
+import com.finco.account.IAccount;
+import com.finco.account.IAccountFactory;
+import com.finco.customer.ICustomer;
+
+public class GoldFactory implements IAccountFactory{
+
+	@Override
+	public IAccount getAccount(ICustomer customer, String accountNumber,
+			double balance) {
+		// TODO Auto-generated method stub
+		return new GoldAccount(customer, accountNumber, balance);
+	}
 
 }
